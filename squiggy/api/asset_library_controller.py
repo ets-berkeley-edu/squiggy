d7ba0ffb34d304fd759f9d2f6f78eb735ae17732
John Crossman <johncrossman@berkeley.edu>
2021-03-02 15:02:52 -0800

    COL-1449, infinite scroll through (mock) asset /list

--- squiggy/api/asset_library_controller.py
+++ squiggy/api/asset_library_controller.py
@@ -23,6 +23,7 @@ SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED
 ENHANCEMENTS, OR MODIFICATIONS.
 """
 
+from copy import copy
 import json
 
 from flask import current_app as app, request
@@ -34,25 +35,37 @@ from squiggy.lib.http import tolerant_jsonify
 @login_required
 def assets(domain, course_site_id):
     args = {
-        'category': _get(request.args, 'category', 0),
-        'hasComments': _get(request.args, 'hasComments', 0),
-        'hasImpact': _get(request.args, 'hasImpact', 0),
-        'hasLikes': _get(request.args, 'hasLikes', 0),
-        'hasPins': _get(request.args, 'hasPins', 0),
-        'hasTrending': _get(request.args, 'hasTrending', 0),
-        'hasViews': _get(request.args, 'hasViews', 0),
-        'keywords': _get(request.args, 'keywords', 0),
-        'limit': _get(request.args, 'limit', 0),
+        'category': _get(request.args, 'category', None),
+        'hasComments': _get(request.args, 'hasComments', None),
+        'hasImpact': _get(request.args, 'hasImpact', None),
+        'hasLikes': _get(request.args, 'hasLikes', None),
+        'hasPins': _get(request.args, 'hasPins', None),
+        'hasTrending': _get(request.args, 'hasTrending', None),
+        'hasViews': _get(request.args, 'hasViews', None),
+        'keywords': _get(request.args, 'keywords', None),
+        'limit': _get(request.args, 'limit', 10),
         'offset': _get(request.args, 'offset', 0),
-        'searchContext': _get(request.args, 'searchContext', 0),
-        'section': _get(request.args, 'section', 0),
-        'sort': _get(request.args, 'sort', 0),
-        'type': _get(request.args, 'type', 0),
-        'user': _get(request.args, 'user', 0),
+        'searchContext': _get(request.args, 'searchContext', None),
+        'section': _get(request.args, 'section', None),
+        'sort': _get(request.args, 'sort', 'TODO'),
+        'type': _get(request.args, 'type', None),
+        'user': _get(request.args, 'user', None),
     }
     mock_json = _load_json(f'fixtures/mock_data_source/assets-{domain}-{course_site_id}.json')
-    mock_json['offset'] = args['offset']
-    return tolerant_jsonify(mock_json)
+    range_start = int(args['offset'])
+    total = mock_json['total']
+    api_json = {
+        'offset': range_start,
+        'results': [],
+        'total': total,
+    }
+    range_stop = range_start + int(args['limit'])
+    range_stop = range_stop if range_stop < total else total
+    for n in range(range_start, range_stop):
+        item = copy(mock_json['results'][n % 10])
+        item['id'] = range_start + n + 1
+        api_json['results'].append(item)
+    return tolerant_jsonify(api_json)
 
 
 def _get(_dict, key, default_value=None):
@@ -65,3 +78,108 @@ def _load_json(path):
         return json.load(file)
     except (FileNotFoundError, KeyError, TypeError):
         return None
+
+
+def _hot_100_of_1973():
+    return {
+        {'title': 'Tie a Yellow Ribbon Round the Ole Oak Tree', 'artist': 'Tony Orlando and Dawn'},
+        {'title': 'Bad, Bad Leroy Brown', 'artist': 'Jim Croce'},
+        {'title': 'Killing Me Softly with His Song', 'artist': 'Roberta Flack'},
+        {'title': 'Let\'s Get It On', 'artist': 'Marvin Gaye'},
+        {'title': 'My Love', 'artist': 'Paul McCartney & Wings'},
+        {'title': 'Why Me', 'artist': 'Kris Kristofferson'},
+        {'title': 'Crocodile Rock', 'artist': 'Elton John'},
+        {'title': 'Will It Go Round in Circles', 'artist': 'Billy Preston'},
+        {'title': 'You\'re So Vain', 'artist': 'Carly Simon'},
+        {'title': 'Touch Me in the Morning', 'artist': 'Diana Ross'},
+        {'title': 'The Night the Lights Went Out in Georgia', 'artist': 'Vicki Lawrence'},
+        {'title': 'Playground in My Mind', 'artist': 'Clint Holmes'},
+        {'title': 'Brother Louie', 'artist': 'Stories'},
+        {'title': 'Delta Dawn', 'artist': 'Helen Reddy'},
+        {'title': 'Me and Mrs. Jones', 'artist': 'Billy Paul'},
+        {'title': 'Frankenstein', 'artist': 'The Edgar Winter Group'},
+        {'title': 'Drift Away', 'artist': 'Dobie Gray'},
+        {'title': 'Little Willy', 'artist': 'Sweet'},
+        {'title': 'You Are the Sunshine of My Life', 'artist': 'Stevie Wonder'},
+        {'title': 'Half-Breed', 'artist': 'Cher'},
+        {'title': 'That Lady', 'artist': 'The Isley Brothers'},
+        {'title': 'Pillow Talk', 'artist': 'Sylvia Robinson'},
+        {'title': 'We\'re an American Band', 'artist': 'Grand Funk Railroad'},
+        {'title': 'Right Place Wrong Time', 'artist': 'Dr. John'},
+        {'title': 'Wildflower', 'artist': 'Skylark'},
+        {'title': 'Superstition', 'artist': 'Stevie Wonder'},
+        {'title': 'Loves Me Like a Rock', 'artist': 'Paul Simon'},
+        {'title': 'The Morning After', 'artist': 'Maureen McGovern'},
+        {'title': 'Rocky Mountain High', 'artist': 'John Denver'},
+        {'title': 'Stuck in the Middle with You', 'artist': 'Stealers Wheel'},
+        {'title': 'Shambala', 'artist': 'Three Dog Night'},
+        {'title': 'Love Train', 'artist': 'The O\'Jays'},
+        {'title': 'I\'m Gonna Love You Just a Little More Baby', 'artist': 'Barry White'},
+        {'title': 'Say, Has Anybody Seen My Sweet Gypsy Rose', 'artist': 'Tony Orlando and Dawn'},
+        {'title': 'Keep on Truckin\'', 'artist': 'Eddie Kendricks'},
+        {'title': 'Danny\'s Song', 'artist': 'Anne Murray'},
+        {'title': 'Dancing in the Moonlight', 'artist': 'King Harvest'},
+        {'title': 'Monster Mash', 'artist': 'Bobby Pickett'},
+        {'title': 'Natural High', 'artist': 'Bloodstone'},
+        {'title': 'Diamond Girl', 'artist': 'Seals and Crofts'},
+        {'title': 'Long Train Runnin\'', 'artist': 'The Doobie Brothers'},
+        {'title': 'Give Me Love (Give Me Peace on Earth)', 'artist': 'George Harrison'},
+        {'title': 'If You Want Me to Stay', 'artist': 'Sly & the Family Stone'},
+        {'title': 'Daddy\'s Home', 'artist': 'Jermaine Jackson'},
+        {'title': 'Neither One of Us (Wants to Be the First to Say Goodbye)', 'artist': 'Gladys Knight & the Pips'},
+        {'title': 'I\'m Doin\' Fine Now', 'artist': 'New York City'},
+        {'title': 'Could It Be I\'m Falling in Love', 'artist': 'The Spinners'},
+        {'title': 'Daniel', 'artist': 'Elton John'},
+        {'title': 'Midnight Train to Georgia', 'artist': 'Gladys Knight & the Pips'},
+        {'title': 'Smoke on the Water', 'artist': 'Deep Purple'},
+        {'title': 'The Cover of the Rolling Stone', 'artist': 'Dr. Hook & The Medicine Show'},
+        {'title': 'Behind Closed Doors', 'artist': 'Charlie Rich'},
+        {'title': 'Your Mama Don\'t Dance', 'artist': 'Loggins and Messina'},
+        {'title': 'Feelin\' Stronger Every Day', 'artist': 'Chicago'},
+        {'title': 'The Cisco Kid', 'artist': 'War'},
+        {'title': 'Live and Let Die', 'artist': 'Paul McCartney & Wings'},
+        {'title': 'Oh, Babe, What Would You Say?', 'artist': 'Hurricane Smith'},
+        {'title': 'I Believe in You (You Believe in Me)', 'artist': 'Johnnie Taylor'},
+        {'title': 'Sing', 'artist': 'The Carpenters'},
+        {'title': 'Ain\'t No Woman (Like the One I\'ve Got)', 'artist': 'The Four Tops'},
+        {'title': 'Dueling Banjos', 'artist': 'Eric Weissberg & Steve Mandell'},
+        {'title': 'Higher Ground', 'artist': 'Stevie Wonder'},
+        {'title': 'Here I Am (Come and Take Me)', 'artist': 'Al Green'},
+        {'title': 'My Maria', 'artist': 'B.W. Stevenson'},
+        {'title': 'Superfly', 'artist': 'Curtis Mayfield'},
+        {'title': 'Last Song', 'artist': 'Edward Bear'},
+        {'title': 'Get Down', 'artist': 'Gilbert O\'Sullivan'},
+        {'title': 'Reelin\' in the Years', 'artist': 'Steely Dan'},
+        {'title': 'Hocus Pocus', 'artist': 'Focus'},
+        {'title': 'Yesterday Once More', 'artist': 'The Carpenters'},
+        {'title': 'Boogie Woogie Bugle Boy', 'artist': 'Bette Midler'},
+        {'title': 'Clair', 'artist': 'Gilbert O\'Sullivan'},
+        {'title': 'Do It Again', 'artist': 'Steely Dan'},
+        {'title': 'Kodachrome', 'artist': 'Paul Simon'},
+        {'title': 'Why Can\'t We Live Together', 'artist': 'Timmy Thomas'},
+        {'title': 'Do You Wanna Dance?', 'artist': 'Bette Midler'},
+        {'title': 'So Very Hard to Go', 'artist': 'Tower of Power'},
+        {'title': 'Rockin\' Pneumonia and the Boogie Woogie Flu', 'artist': 'Johnny Rivers'},
+        {'title': 'Ramblin\' Man', 'artist': 'The Allman Brothers Band'},
+        {'title': 'Masterpiece', 'artist': 'The Temptations'},
+        {'title': 'Peaceful', 'artist': 'Helen Reddy'},
+        {'title': 'One of a Kind (Love Affair)', 'artist': 'The Spinners'},
+        {'title': 'Funny Face', 'artist': 'Donna Fargo'},
+        {'title': 'Funky Worm', 'artist': 'Ohio Players'},
+        {'title': 'Angie', 'artist': 'The Rolling Stones'},
+        {'title': 'Jambalaya (On the Bayou)', 'artist': 'Blue Ridge Rangers'},
+        {'title': 'Don\'t Expect Me to Be Your Friend', 'artist': 'Lobo'},
+        {'title': 'Break Up to Make Up', 'artist': 'The Stylistics'},
+        {'title': 'Daisy a Day', 'artist': 'Jud Strunk'},
+        {'title': 'Also Sprach Zarathustra (2001)', 'artist': 'Deodato'},
+        {'title': 'Stir It Up', 'artist': 'Johnny Nash'},
+        {'title': 'Money', 'artist': 'Pink Floyd'},
+        {'title': 'Gypsy Man', 'artist': 'War'},
+        {'title': 'The World Is a Ghetto', 'artist': 'War'},
+        {'title': 'Yes We Can Can', 'artist': 'The Pointer Sisters'},
+        {'title': 'Free Ride', 'artist': 'The Edgar Winter Group'},
+        {'title': 'Space Oddity', 'artist': 'David Bowie'},
+        {'title': 'It Never Rains in Southern California', 'artist': 'Albert Hammond'},
+        {'title': 'The Twelfth of Never', 'artist': 'Donny Osmond'},
+        {'title': 'Papa Was a Rollin\' Stone', 'artist': 'The Temptations'},
+    }
--- src/api/assets.ts
+++ src/api/assets.ts
@@ -1,6 +1,9 @@
+import _ from 'lodash'
 import axios from 'axios'
 import utils from '@/api/api-utils'
 
-export function getAssets(domain, courseSiteId) {
-  return axios.get(`${utils.apiBaseUrl()}/api/${domain}/${courseSiteId}/assets`)
+export function getAssets(domain, courseSiteId, args={}) {
+  let url = `${utils.apiBaseUrl()}/api/${domain}/${courseSiteId}/assets?`
+  _.each(args, (value, key) => url += `${key}=${value}&`)
+  return axios.get(url)
 }
