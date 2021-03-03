6e490477d49890d62de34dac28ee1268c55cb82e
John Crossman <johncrossman@berkeley.edu>
2021-03-02 22:26:51 -0800

    COL-1450/COL-1458, clean up /assets and /devauth; font-awesome over mdi-icon

---
+++ src/components/assets/AssetCard.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-card>
+    <v-sheet elevation="1">
+      <v-img src="@/assets/mock-asset-preview.png">
+        <v-card-text class="asset-metadata">
+          <div class="mb-3">
+            {{ asset.title }}
+          </div>
+          <div>
+            by {{ oxfordJoin($_.map(asset.users, 'canvasFullName')) }}
+          </div>
+        </v-card-text>
+      </v-img>
+    </v-sheet>
+    <v-card-actions>
+      <div class="align-center d-flex justify-space-between w-100">
+        <div>
+          <v-btn :id="`iconbar-pin-${asset.id}`" icon>
+            <font-awesome-icon icon="thumbtack">
+              <span v-if="!asset.isPinnedByMe" class="sr-only">Pin</span>
+              <span v-if="asset.isPinnedByMe" class="sr-only">Pinned</span>
+            </font-awesome-icon>
+          </v-btn>
+        </div>
+        <div>
+          <div class="align-center d-flex">
+            <div class="pr-3">
+              <font-awesome-icon icon="thumbs-up" />
+              {{ asset.likes }}
+            </div>
+            <div class="pr-3">
+              <font-awesome-icon icon="eye" />
+              {{ asset.views }}
+            </div>
+            <div>
+              <font-awesome-icon icon="comment" />
+              {{ asset.commentCount }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import Utils from '@/mixins/Utils'
+
+export default {
+  name: 'AssetCard',
+  mixins: [Utils],
+  props: {
+    asset: {
+      required: true,
+      type: Object
+    }
+  }
+}
+</script>
+
+<style scoped>
+.asset-metadata {
+  background-color: #333;
+  background-color: rgba(51, 51, 51, 0.9);
+  bottom: 0;
+  color: #FFF;
+  left: 0;
+  position: absolute;
+  right: 0;
+}
+.asset-metadata span {
+  font-size: 14px;
+  font-weight: 400;
+}
+.asset-metadata small {
+  font-size: 13px;
+}
+</style>
--- src/components/assets/Assets.vue
+++ src/components/assets/Assets.vue
@@ -1,67 +1,58 @@
 <template>
   <div v-if="!loading">
-    <h1>Showing {{ assets.length }} of {{ total }} Assets</h1>
+    <div class="d-flex justify-space-between pt-2">
+      <div class="w-50">
+        <v-text-field
+          v-model="keywords"
+          class="mb-0"
+          clearable
+          label="Search"
+          solo
+          type="search"
+          @click:append-outer="search"
+        >
+          <template #append>
+            <div class="ml-2">
+              <font-awesome-icon icon="caret-down" />
+            </div>
+          </template>
+          <template #append-outer>
+            <div class="ml-2 mt-1">
+              <font-awesome-icon icon="search" />
+            </div>
+          </template>
+        </v-text-field>
+      </div>
+      <div>
+        <v-btn
+          elevation="2"
+          large
+        >
+          <span class="pr-2">
+            <font-awesome-icon icon="cog" />
+          </span>
+          Manage Assets
+        </v-btn>
+      </div>
+    </div>
     <v-card
       class="d-flex flex-wrap"
       flat
       tile
     >
-      <v-card
+      <AssetCard
         v-for="(asset, $index) in assets"
         :key="$index"
+        :asset="asset"
         class="ma-3"
-      >
-        <v-sheet elevation="1">
-          <v-img src="@/assets/mock-asset-preview.png">
-            <v-card-text class="asset-metadata">
-              <div>
-                {{ asset.title }}
-              </div>
-              <div>
-                by {{ oxfordJoin($_.map(asset.users, 'canvasFullName')) }}
-              </div>
-            </v-card-text>
-          </v-img>
-        </v-sheet>
-        <v-card-actions>
-          <div class="d-flex justify-space-between">
-            <div>
-              <button :id="`iconbar-pin-${asset.id}`">
-                <v-icon>
-                  mdi-pin
-                  <span v-if="!asset.isPinnedByMe" class="sr-only">Pin</span>
-                  <span v-if="asset.isPinnedByMe" class="sr-only">Pinned</span>
-                </v-icon>
-              </button>
-            </div>
-            <div>
-              <v-btn :id="`iconbar-like-${asset.id}`">
-                <v-icon>
-                  mdi-thumb-up-outline
-                  <span class="sr-only">Like</span>
-                </v-icon>
-              </v-btn>
-              {{ asset.likes }}
-              <v-icon>
-                mdi-eye-outline
-                <span class="sr-only">Views</span>
-              </v-icon>
-              {{ asset.views }}
-              <v-icon>
-                mdi-comment-outline
-                <span class="sr-only">Comments</span>
-              </v-icon>
-              {{ asset.commentCount }}
-            </div>
-          </div>
-        </v-card-actions>
-      </v-card>
+      />
     </v-card>
     <InfiniteLoading spinner="waveDots" @infinite="fetchMore" />
   </div>
 </template>
 
 <script>
+import AssetCard from '@/components/assets/AssetCard'
 import Context from '@/mixins/Context'
 import InfiniteLoading from 'vue-infinite-loading'
 import Utils from '@/mixins/Utils'
@@ -69,10 +60,11 @@ import {getAssets} from '@/api/assets'
 
 export default {
   name: 'Assets',
-  components: {InfiniteLoading},
+  components: {AssetCard, InfiniteLoading},
   mixins: [Context, Utils],
   data: () => ({
     assets: [],
+    keywords: undefined,
     limit: 10,
     offset: 0,
     total: undefined
@@ -101,31 +93,10 @@ export default {
     },
     fetchMore($state) {
       this.fetch().then(data => data.results.length ? $state.loaded() : $state.complete())
+    },
+    search() {
+      console.log('TODO: Search')
     }
   }
 }
 </script>
-
-<style scoped>
-.asset-metadata {
-  background-color: #333;
-  background-color: rgba(51, 51, 51, 0.9);
-  bottom: 0;
-  color: #FFF;
-  left: 0;
-  position: absolute;
-  right: 0;
-}
-
-.asset-metadata span {
-  font-size: 14px;
-  font-weight: 400;
-  line-height: 1.3;
-  margin-bottom: 5px;
-  margin-top: 0;
-}
-
-.asset-metadata small {
-  font-size: 13px;
-}
-</style>
