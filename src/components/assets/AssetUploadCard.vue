552f58bd6cf5584c23b8f4a168ffac41d978ddb7
John Crossman <johncrossman@berkeley.edu>
2021-03-03 15:25:17 -0800

    COL-1456, Upload/Add asset tile; new page at '/asset/add/link'

---
+++ src/components/assets/AssetUploadCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-card class="mb-3" no-gutters>
+    <v-sheet class="fill-height">
+      <v-container class="card-container fill-height px-5 text-center" fluid>
+        <v-row>
+          <v-col cols="18" sm="6">
+            <v-btn block class="w-100" height="300">
+              <div class="d-flex flex-column">
+                <div class="pb-2">
+                  <font-awesome-icon icon="laptop" size="lg" />
+                </div>
+                <div>
+                  Upload
+                </div>
+              </div>
+            </v-btn>
+          </v-col>
+          <v-col cols="18" sm="6">
+            <v-btn
+              block
+              class="w-100"
+              height="300"
+              @click="go('/asset/add/link')"
+            >
+              <div class="d-flex flex-column">
+                <div class="pb-2">
+                  <font-awesome-icon icon="link" size="lg" />
+                </div>
+                <div>
+                  Add Link
+                </div>
+              </div>
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-sheet>
+  </v-card>
+</template>
+
+<script>
+import Utils from '@/mixins/Utils'
+
+export default {
+  name: 'AssetUploadCard',
+  mixins: [Utils]
+}
+</script>
+
+<style scoped>
+.card-container {
+  min-width: 300px;
+  align-content: center;
+}
+</style>
