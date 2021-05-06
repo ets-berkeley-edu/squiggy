8327cdd689655c2bce1383340c0ec0b817e8d299
Pauline Kerschen <kerschen@berkeley.edu>
2021-05-06 14:25:06 -0700

    COL-1558 Boxplot for Engagement Index student view

--- src/components/engage/Engage.vue
+++ src/components/engage/Engage.vue
@@ -19,9 +19,11 @@
         <div class="engagement-userinfo-badge-title">My Points</div>
         <div id="engagement-userinfo-points" class="engagement-userinfo-badge-data">{{ $currentUser.points }}</div>
       </div>
-      <div v-if="showLeaderboard" class="engagement-userinfo-badge engagement-userinfo-boxplot-container">
+      <div v-if="showLeaderboard && boxplotOptions" class="engagement-userinfo-badge engagement-userinfo-boxplot-container">
         <div class="engagement-userinfo-badge-title">How do I compare</div>
-        <div id="engagement-userinfo-boxplot" class="engagement-userinfo-boxplot" />
+        <div id="engagement-userinfo-boxplot" class="engagement-userinfo-boxplot">
+          <highcharts :options="boxplotOptions" />
+        </div>
       </div>
     </div>
 
@@ -55,6 +57,7 @@ export default {
   components: {Leaderboard},
   data() {
     return {
+      boxplotOptions: null,
       leaderboard: null,
       rank: null,
       sharePoints: this.setInitialSharePoints(),
@@ -65,11 +68,139 @@ export default {
     this.refreshLeaderboard()
   },
   methods: {
+    calculateBoxPlotData(series) {
+      series.sort((a, b) => b - a)
+      const min = series[0]
+      const max = series[series.length - 1]
+      const q1 = this.calculateQuartile(series, 1)
+      const q2 = this.calculateQuartile(series, 2)
+      const q3 = this.calculateQuartile(series, 3)
+      return [
+        max,
+        q3,
+        q2,
+        q1,
+        min
+      ]
+    },
+    calculateQuartile(series, quartile) {
+      const quartileIndex = Math.floor(series.length / 4 * quartile)
+      // If the quartile point lands in between 2 items, calculate the average of those items
+      if (series.length % 2 === 1 && series[quartileIndex - 1]) {
+        return (series[quartileIndex - 1] + series[quartileIndex]) / 2
+      // If the quartile point lands on an item in the series, return that value
+      } else {
+        return series[quartileIndex]
+      }
+    },
     rankLeaderboard() {
       for (const [index, row] of this.leaderboard.entries()) {
         row.rank = index + 1
         if (row.id === this.$currentUser.id) {
           this.rank = row.rank
+        }
+      }
+    },
+    refreshBoxplot() {
+      const points = this.leaderboard.map(e => e.points)
+      const boxplotData = this.calculateBoxPlotData(points)
+
+      // No boxplot without sufficient unique data points.
+      if (this.$_.uniq(boxplotData).length < 2) {
+        this.boxplotOptions = null
+        return
+      }
+
+      this.boxplotOptions = {
+        chart: {
+          backgroundColor: 'transparent',
+          height: 80,
+          inverted: true,
+          margin: [0, 20, 0, 20],
+          type: 'boxplot'
+        },
+        credits: {
+          enabled: false
+        },
+        legend: {
+          enabled: false
+        },
+        plotOptions: {
+          boxplot: {
+            color: '#88acc4',
+            fillColor: '#88acc4',
+            lineWidth: 1,
+            medianColor: '#eee',
+            medianWidth: 3,
+            whiskerLength: 20,
+            whiskerWidth: 3
+          }
+        },
+        series: [
+          {
+            data: [boxplotData],
+            pointWidth: 40,
+            tooltip: {
+              borderColor: 'transparent',
+              headerFormat: '',
+              pointFormat: 'Maximum: {point.high}<br/>' +
+                'Upper Quartile: {point.q3}<br/>' +
+                'Median: {point.median}<br/>' +
+                'Lower Quartile: {point.q1}<br/>' +
+                'Minimum: {point.low}'
+            }
+          },
+          {
+            data: [[0, this.$currentUser.points]],
+            marker: {
+              fillColor: '#3179bc',
+              lineWidth: 5,
+              lineColor: '#3179bc'
+            },
+            tooltip: {
+              headerFormat: '',
+              pointFormat: 'My Points: {point.y}'
+            },
+            type: 'scatter'
+          }
+        ],
+        title: {
+          text: ''
+        },
+        tooltip: {
+          backgroundColor: '#000',
+          distance: 30,
+          hideDelay: 100,
+          outside: true,
+          shadow: false,
+          style: {
+            color: '#fff'
+          },
+          useHTML: true
+        },
+        xAxis: {
+          endOnTick: false,
+          labels: {
+            enabled: false
+          },
+          lineWidth: 0,
+          startOnTick: false,
+          tickLength: 0
+        },
+        yAxis: {
+          endOnTick: false,
+          gridLineWidth: 0,
+          labels: {
+            enabled: false
+          },
+          lineWidth: 0,
+          maxPadding: 0,
+          minPadding: 0,
+          startOnTick: false,
+          tickLength: 0,
+          title: {
+            enabled: false
+          }
         }
       }
     },
@@ -79,6 +210,7 @@ export default {
         getLeaderboard().then((data) => {
           this.leaderboard = data
           this.rankLeaderboard()
+          this.refreshBoxplot()
           this.showLeaderboard = true
           this.$ready('Engagement Index')
         })
