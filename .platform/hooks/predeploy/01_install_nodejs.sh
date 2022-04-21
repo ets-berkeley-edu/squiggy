890d3a406043738895af04306144c2017c197a38
John Crossman <johncrossman@berkeley.edu>
2022-04-21 13:14:26 -0700

    COL-1757, use sudo in 01_install_nodejs script

--- .platform/hooks/predeploy/01_install_nodejs.sh
+++ .platform/hooks/predeploy/01_install_nodejs.sh
@@ -1,3 +1,7 @@
-curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
-yum install -y nodejs
-npm install -g typescript
+#!/bin/bash
+
+sudo curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
+sudo yum install -y nodejs
+sudo npm install -g typescript
+
+exit 0
