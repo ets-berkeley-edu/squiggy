a8b964c87588a80190fba4ce7f1fe7cb9904a8dd
John Crossman <johncrossman@berkeley.edu>
2022-04-29 13:29:24 -0700

    COL-1743, handle /api/previews/whiteboard/callback; persist thumbnail, image_url

--- squiggy/api/previews_controller.py
+++ squiggy/api/previews_controller.py
@@ -32,10 +32,20 @@ from squiggy.lib.previews import verify_preview_service_authorization
 from squiggy.lib.util import utc_now
 from squiggy.logger import logger
 from squiggy.models.asset import Asset
+from squiggy.models.whiteboard import Whiteboard
 
 
 @app.route('/api/previews/callback', methods=['POST'])
 def previews_callback():
+    return _handle_previews_callback('asset')
+
+
+@app.route('/api/previews/whiteboard/callback', methods=['POST'])
+def whiteboard_previews_callback():
+    return _handle_previews_callback('whiteboard')
+
+
+def _handle_previews_callback(object_type):
     if not verify_preview_service_authorization(request.headers.get('authorization')):
         raise UnauthorizedRequestError('Missing or invalid authorization header.')
 
@@ -54,17 +64,39 @@ def previews_callback():
         logger.exception(e)
         raise BadRequestError('Could not parse JSON metadata.')
 
-    asset = Asset.find_by_id(params['id'])
-    if not asset:
-        raise BadRequestError(f"Asset {params['id']} not found.")
+    success = False
+    if object_type == 'asset':
+        success = _update_asset_preview(metadata=metadata, params=params)
+    elif object_type == 'whiteboard':
+        success = _update_whiteboard_preview(params=params)
+    if success:
+        return tolerant_jsonify({'status': 'success'})
+    else:
+        raise InternalServerError(f"Unable to update preview data ({object_type}_id={params['id']}.")
 
-    if asset.update_preview(
+
+def _update_asset_preview(metadata, params):
+    asset_id = params['id']
+    asset = Asset.find_by_id(asset_id)
+    if not asset:
+        raise BadRequestError(f'Asset {asset_id} not found.')
+
+    return asset.update_preview(
         preview_status=params.get('status'),
         thumbnail_url=params.get('thumbnail'),
         image_url=params.get('image'),
         pdf_url=params.get('pdf'),
         metadata=metadata,
-    ):
-        return tolerant_jsonify({'status': 'success'})
-    else:
-        raise InternalServerError(f"Unable to update preview data (asset_id={params['id']}.")
+    )
+
+
+def _update_whiteboard_preview(params):
+    whiteboard_id = params['id']
+    whiteboard = Whiteboard.find_by_id(whiteboard_id)
+    if not whiteboard:
+        raise BadRequestError(f'Whiteboard {whiteboard_id} not found.')
+
+    return whiteboard.update_preview(
+        image_url=params.get('image'),
+        thumbnail_url=params.get('thumbnail'),
+    )
