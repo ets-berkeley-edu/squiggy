4b1775be8988e6693703eb3a18ca84ed1ff1aecb
John Crossman <johncrossman@berkeley.edu>
2022-06-22 13:32:29 -0700

    COL-1883, remove extraneous Apache configs

--- .platform/hooks/postdeploy/01_update_apache_config.sh
+++ .platform/hooks/postdeploy/01_update_apache_config.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-sudo mv /tmp/ssl.conf /etc/httpd/conf.d/elasticbeanstalk/00_application.conf
+
+sudo mv /tmp/web_sockets.conf /etc/httpd/conf.d/elasticbeanstalk/00_application.conf
 sudo /bin/systemctl restart httpd.service
+
+exit 0
