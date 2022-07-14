868f84cbc18cd315b1d82e46cba5147032a64e60
John Crossman <johncrossman@berkeley.edu>
2022-07-14 11:53:18 -0700

    COL-1914, developer debug shell script to generate_whiteboard_png per whiteboard_id

--- scripts/compile_whiteboard_to_png.sh
+++ scripts/compile_whiteboard_to_png.sh
@@ -7,13 +7,9 @@ set -e
 npm list | grep typescript || npm install -g typescript
 npm list | grep fabric || npm install -g fabric
 
-echo; echo 'Compiling...'; echo
-
 cd "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/.."
 
 # When deploying to AWS Elastic Beanstalk, this compilation happens in buildspec.yml.
 tsc --resolveJsonModule --esModuleInterop scripts/node_js/save_whiteboard_as_png.ts
-
-echo; echo 'Done.'; echo
 
 exit 0
