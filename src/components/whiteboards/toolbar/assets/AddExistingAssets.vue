8616619c174fbf67042daf4ab238b2ac3bda9591
John Crossman <johncrossman@berkeley.edu>
2022-11-14 12:01:47 -0800

    COL-2120, /whiteboard: 'Add Existing Assets' search is cancellable

--- src/components/whiteboards/toolbar/assets/AddExistingAssets.vue
+++ src/components/whiteboards/toolbar/assets/AddExistingAssets.vue
@@ -30,49 +30,62 @@
         <h2 id="modal-header" class="title">Select Asset(s)</h2>
       </v-card-title>
       <v-divider></v-divider>
-      <v-card-text>
-        <div v-if="totalAssetCount === 0">
-          <h3 id="no-existing-assets" class="px-3 py-8">There are no existing assets to add.</h3>
-        </div>
+      <v-card-text class="mt-3">
         <AssetsHeader
-          v-if="totalAssetCount > 1"
+          :after-reset="() => {
+            resetAddExistingAssets()
+            init()
+          }"
+          :disable-bookmarkable="true"
           :hide-manage-assets-button="true"
           :open-advanced-search="!!(assetType || categoryId || (orderBy !== orderByDefault) || userId)"
           put-focus-on-load="basic-search-input"
         />
-        <v-card-actions v-if="isDialogReady" class="pt-5">
-          <div class="font-weight-bold pl-3 text--secondary">
-            {{ selectedAssetIds.length }} selected
-          </div>
-          <v-spacer></v-spacer>
-          <div class="pb-3 pr-2">
-            <v-btn
-              id="save-btn"
-              color="primary"
-              :disabled="disableSave"
-              @click="save"
-            >
-              <span v-if="isSaving" class="mt-1">
-                <v-progress-circular
-                  class="mr-1"
-                  :indeterminate="true"
-                  rotate="5"
-                  size="18"
-                  width="2"
-                />
-                Saving
-              </span>
-              <span v-if="!isSaving">Save</span>
-            </v-btn>
-            <v-btn
-              id="cancel-btn"
-              text
-              @click="cancel"
-            >
-              Cancel
-            </v-btn>
-          </div>
-        </v-card-actions>
+        <v-container v-if="isDialogReady" class="mt-3" fluid>
+          <v-row align="center">
+            <v-col cols="3">
+              <div
+                v-if="selectedAssetIds.length || totalAssetCount"
+                class="font-weight-bold text--secondary"
+              >
+                {{ pluralize('asset', selectedAssetIds.length, {0: 'No'}) }} selected.
+              </div>
+            </v-col>
+            <v-col cols="9">
+              <div class="float-end">
+                <v-btn
+                  id="save-btn"
+                  class="mr-2"
+                  color="primary"
+                  :disabled="disableSave"
+                  @click="save"
+                >
+                  <span v-if="isSaving" class="mt-1">
+                    <v-progress-circular
+                      class="mr-1"
+                      :indeterminate="true"
+                      rotate="5"
+                      size="18"
+                      width="2"
+                    />
+                    Saving
+                  </span>
+                  <span v-if="!isSaving">
+                    Add Assets
+                  </span>
+                </v-btn>
+                <v-btn
+                  id="cancel-btn"
+                  elevation="1"
+                  medium
+                  @click="cancel"
+                >
+                  Cancel
+                </v-btn>
+              </div>
+            </v-col>
+          </v-row>
+        </v-container>
         <v-card class="d-flex flex-wrap" flat tile>
           <div
             v-for="(asset, index) in assetGrid"
@@ -150,37 +163,22 @@ export default {
     }
   },
   watch: {
-    dialog(value) {
+    dialog() {
       this.resetSearch()
       this.stopInfiniteLoading()
       this.isComplete = false
       this.selectedAssetIds = []
-      if (value) {
-        this.initAssetSearchOptions().then(() => {
-          this.search().then(() => {
-            if (!this.totalAssetCount) {
-              this.isComplete = true
-            } else {
-              this.checkAssetsLoaded()
-              if (!this.isComplete) {
-                this.startInfiniteLoading(this.fetch, {dialog: true, threshold: 800})
-              }
-            }
-            this.isDialogReady = true
-            this.$putFocusNextTick('modal-header')
-          })
-        })
-      } else {
-        this.reset()
-      }
+      this.resetAddExistingAssets()
+      this.init()
     }
   },
   methods: {
     cancel() {
       this.$announcer.polite('Canceled.')
-      this.reset()
       this.dialog = false
       this.isDialogReady = false
+      this.resetAddExistingAssets()
+      this.init()
     },
     checkAssetsLoaded() {
       if (!this.isComplete && this.$_.size(this.assets) >= this.totalAssetCount) {
@@ -199,6 +197,22 @@ export default {
       })
     },
     getSkeletons: count => Array.from(new Array(count), () => ({isLoading: true})),
+    init() {
+      this.initAssetSearchOptions().then(() => {
+        this.search().then(() => {
+          if (!this.totalAssetCount) {
+            this.isComplete = true
+          } else {
+            this.checkAssetsLoaded()
+            if (!this.isComplete) {
+              this.startInfiniteLoading(this.fetch, {dialog: true, threshold: 800})
+            }
+          }
+          this.isDialogReady = true
+          this.$putFocusNextTick('modal-header')
+        })
+      })
+    },
     onClickAssetImage(asset) {
       if (this.selectedAssetIds.includes(asset.id)) {
         this.selectedAssetIds = this.$_.filter(this.selectedAssetIds, id => id !== asset.id)
@@ -206,11 +220,18 @@ export default {
         this.selectedAssetIds.push(asset.id)
       }
     },
-    reset() {
+    resetAddExistingAssets() {
       this.isComplete = false
       this.isDialogReady = false
       this.isSaving = false
       this.selectedAssetIds = []
+      this.setAssetType(null)
+      this.setCategoryId(null)
+      this.setGroupId(null)
+      this.setKeywords(undefined)
+      this.setOrderBy(this.orderByDefault)
+      this.setUserId(null)
+      this.setSection(null)
       this.stopInfiniteLoading()
     },
     save() {
@@ -224,7 +245,7 @@ export default {
           this.$announcer.polite('Assets added')
           this.isSaving = false
           this.dialog = false
-          this.reset()
+          this.resetAddExistingAssets()
         })
       }
     }
