aec888b45a5f56d60fe2ce3a5ea16cc121e042bb
John Crossman <johncrossman@berkeley.edu>
2022-11-15 11:18:27 -0800

    COL-1819, remove obsolete col refs and tidy SQL of hasenpfeffer scripts

--- scripts/hasenpfeffer_incorporated/pull-data.sh
+++ scripts/hasenpfeffer_incorporated/pull-data.sh
@@ -75,7 +75,7 @@ output_csv() {
   # Connect to the source database and pipe the results of a supplied query to a CSV file in the local directory.
   echo "Copying ${1} from database..."
   PGPASSWORD=${db_password} psql -h "${db_host}" -p "${db_port}" -d "${db_database}" --username "${db_username}" \
-  -c "copy (${2}) to stdout with (format csv, header true, force_quote *, delimiter '|')" > "${CSV_HOME_DIRECTORY}/${1}.csv"
+  -c "COPY (${2}) TO STDOUT WITH (FORMAT CSV, HEADER TRUE, FORCE_QUOTE *, DELIMITER '|')" > "${CSV_HOME_DIRECTORY}/${1}.csv"
 }
 
 # Query each table for data associated with the supplied Canvas instance. The only table we do not query is the
@@ -86,68 +86,68 @@ output_csv() {
 
 if [[ "${source_canvas}" ]]; then
 
-  output_csv "activities" "select a.* from activities a
-              join courses c
-              on a.course_id = c.id and c.canvas_api_domain = '${source_canvas}'
+  output_csv "activities" "SELECT a.* FROM activities a
+              JOIN courses c
+              ON a.course_id = c.id AND c.canvas_api_domain = '${source_canvas}'
               order by id"
 
-  output_csv "activity_types" "select at.* from activity_types at
-              join courses c
-              on at.course_id = c.id and c.canvas_api_domain = '${source_canvas}'"
+  output_csv "activity_types" "SELECT at.* FROM activity_types at
+              JOIN courses c
+              ON at.course_id = c.id AND c.canvas_api_domain = '${source_canvas}'"
 
-  output_csv "asset_categories" "select ac.* from asset_categories ac
-              join (categories cat join courses c
-                on cat.course_id = c.id and c.canvas_api_domain = '${source_canvas}')
-              on ac.category_id = cat.id"
+  output_csv "asset_categories" "SELECT ac.* FROM asset_categories ac
+              JOIN (categories cat JOIN courses c
+                ON cat.course_id = c.id AND c.canvas_api_domain = '${source_canvas}')
+              ON ac.category_id = cat.id"
 
-  output_csv "asset_users" "select au.* from asset_users au
-              join (users u join courses c
-                on u.course_id = c.id and c.canvas_api_domain = '${source_canvas}')
-              on au.user_id = u.id"
+  output_csv "asset_users" "SELECT au.* FROM asset_users au
+              JOIN (users u JOIN courses c
+                ON u.course_id = c.id AND c.canvas_api_domain = '${source_canvas}')
+              ON au.user_id = u.id"
 
-  output_csv "categories" "select cat.* from categories cat
-              join courses c
-              on cat.course_id = c.id and c.canvas_api_domain = '${source_canvas}'"
+  output_csv "categories" "SELECT cat.* FROM categories cat
+              JOIN courses c
+              ON cat.course_id = c.id AND c.canvas_api_domain = '${source_canvas}'"
 
-  output_csv "comments" "select com.* from comments com
-              join (users u join courses c
-                on u.course_id = c.id and c.canvas_api_domain = '${source_canvas}')
-              on com.user_id = u.id"
+  output_csv "comments" "SELECT com.* FROM comments com
+              JOIN (users u JOIN courses c
+                ON u.course_id = c.id AND c.canvas_api_domain = '${source_canvas}')
+              ON com.user_id = u.id"
 
   # TODO: When course-groups feature is live, un-comment the following.
-  #  output_csv "course_group_memberships" "select g.* from course_group_memberships g
-  #              join (users u join courses c
-  #                on g.course_id = c.id and c.canvas_api_domain = '${source_canvas}')
-  #              on g.canvas_user_id = u.canvas_user_id"
+  #  output_csv "course_group_memberships" "SELECT g.* FROM course_group_memberships g
+  #              JOIN (users u JOIN courses c
+  #                ON g.course_id = c.id AND c.canvas_api_domain = '${source_canvas}')
+  #              ON g.canvas_user_id = u.canvas_user_id"
   #
-  #  output_csv "course_groups" "select g.* from course_groups g
-  #              join courses c
-  #              on g.course_id = c.id and c.canvas_api_domain = '${source_canvas}'"
+  #  output_csv "course_groups" "SELECT g.* FROM course_groups g
+  #              JOIN courses c
+  #              ON g.course_id = c.id AND c.canvas_api_domain = '${source_canvas}'"
 
-  output_csv "whiteboard_users" "select wm.* from whiteboard_users wm
-              join (whiteboards w join courses c
-                on w.course_id = c.id and c.canvas_api_domain = '${source_canvas}')
-              on wm.whiteboard_id = w.id"
+  output_csv "whiteboard_users" "SELECT wm.* FROM whiteboard_users wm
+              JOIN (whiteboards w JOIN courses c
+                ON w.course_id = c.id AND c.canvas_api_domain = '${source_canvas}')
+              ON wm.whiteboard_id = w.id"
 
-  output_csv "users" "select u.* from users u
-              join courses c
-              on u.course_id = c.id and c.canvas_api_domain = '${source_canvas}'"
+  output_csv "users" "SELECT u.* FROM users u
+              JOIN courses c
+              ON u.course_id = c.id AND c.canvas_api_domain = '${source_canvas}'"
 
 # If no source Canvas is specified, select all rows.
 
 else
 
-  output_csv "activities" "select * from activities order by id"
-  output_csv "activity_types" "select * from activity_types"
-  output_csv "asset_categories" "select * from asset_categories"
-  output_csv "asset_users" "select * from asset_users"
-  output_csv "categories" "select * from categories"
-  output_csv "comments" "select * from comments"
+  output_csv "activities" "SELECT * FROM activities order by id"
+  output_csv "activity_types" "SELECT * FROM activity_types"
+  output_csv "asset_categories" "SELECT * FROM asset_categories"
+  output_csv "asset_users" "SELECT * FROM asset_users"
+  output_csv "categories" "SELECT * FROM categories"
+  output_csv "comments" "SELECT * FROM comments"
   # TODO: When course-groups feature is live, un-comment the following two lines.
-  #  output_csv "course_group_memberships" "select * from comments"
-  #  output_csv "course_groups" "select * from comments"
-  output_csv "whiteboard_users" "select * from whiteboard_users"
-  output_csv "users" "select * from users"
+  #  output_csv "course_group_memberships" "SELECT * FROM comments"
+  #  output_csv "course_groups" "SELECT * FROM comments"
+  output_csv "whiteboard_users" "SELECT * FROM whiteboard_users"
+  output_csv "users" "SELECT * FROM users"
 
 fi
 
@@ -157,103 +157,102 @@ fi
 
 if [[ "${replacement_canvas}" ]]; then
 
-  output_csv "assets" "select a.id, a.type, a.url,
+  output_csv "assets" "SELECT a.id, a.type, a.url,
                 replace(a.download_url, '${source_canvas}', '${replacement_canvas}') as download_url,
                 a.title, a.canvas_assignment_id, a.description, a.thumbnail_url, a.image_url, a.mime,
                 a.source, a.body, a.likes, a.dislikes, a.views, a.comment_count, a.created_at, a.updated_at,
-                a.deleted_at, a.course_id, a.pdf_url, a.preview_status, a.preview_metadata, a.visible,
-                a.impact_score, a.trending_score
-              from assets a
-              join courses c
-              on a.course_id = c.id and c.canvas_api_domain = '${source_canvas}'"
+                a.deleted_at, a.course_id, a.pdf_url, a.preview_status, a.preview_metadata, a.visible
+              FROM assets a
+              JOIN courses c
+              ON a.course_id = c.id AND c.canvas_api_domain = '${source_canvas}'"
 
-  output_csv "asset_whiteboard_elements" "select awe.uid,
+  output_csv "asset_whiteboard_elements" "SELECT awe.uuid,
                 replace(awe.element::text, '${source_canvas}', '${replacement_canvas}') as element,
                 awe.created_at, awe.updated_at, awe.asset_id, awe.element_asset_id
-              from asset_whiteboard_elements awe
-              join (assets a join courses c
-                on a.course_id = c.id and c.canvas_api_domain = '${source_canvas}')
-              on awe.asset_id = a.id"
+              FROM asset_whiteboard_elements awe
+              JOIN (assets a JOIN courses c
+                ON a.course_id = c.id AND c.canvas_api_domain = '${source_canvas}')
+              ON awe.asset_id = a.id"
 
-  output_csv "courses" "select c.id, c.canvas_course_id, c.enable_upload, c.name,
-                replace(c.assetlibrary_url, '${source_canvas}', '${replacement_canvas}') as assetlibrary_url,
-                replace(c.dashboard_url, '${source_canvas}', '${replacement_canvas}') as dashboard_url,
-                replace(c.engagementindex_url, '${source_canvas}', '${replacement_canvas}') as engagementindex_url,
+  output_csv "courses" "SELECT c.id, c.canvas_course_id, c.enable_upload, c.name,
+                replace(c.asset_library_url, '${source_canvas}', '${replacement_canvas}') as asset_library_url,
+                replace(c.impact_studio_url, '${source_canvas}', '${replacement_canvas}') as impact_studio_url,
+                replace(c.engagement_index_url, '${source_canvas}', '${replacement_canvas}') as engagement_index_url,
                 replace(c.whiteboards_url, '${source_canvas}', '${replacement_canvas}') as whiteboards_url,
                 '${replacement_canvas}' as canvas_api_domain,
                 c.active, c.created_at, c.updated_at, c.enable_daily_notifications, c.enable_weekly_notifications
-              from courses c
-              where c.canvas_api_domain = '${source_canvas}'"
+              FROM courses c
+              WHERE c.canvas_api_domain = '${source_canvas}'"
 
-  output_csv "whiteboards" "select w.id, w.title,
+  output_csv "whiteboards" "SELECT w.id, w.title,
                 replace(w.thumbnail_url, '${source_canvas}', '${replacement_canvas}') as thumbnail_url,
                 replace(w.image_url, '${source_canvas}', '${replacement_canvas}') as image_url,
                 w.created_at, w.updated_at, w.course_id, w.deleted_at
-              from whiteboards w
-              join courses c
-              on w.course_id = c.id and c.canvas_api_domain = '${source_canvas}'"
+              FROM whiteboards w
+              JOIN courses c
+              ON w.course_id = c.id AND c.canvas_api_domain = '${source_canvas}'"
 
-  output_csv "whiteboard_elements" "select we.uid,
+  output_csv "whiteboard_elements" "SELECT we.uuid,
                 replace(we.element::text, '${source_canvas}', '${replacement_canvas}') as element,
                 we.created_at, we.updated_at, we.whiteboard_id, we.asset_id
-              from whiteboard_elements we
-              join (whiteboards w join courses c
-                on w.course_id = c.id and c.canvas_api_domain = '${source_canvas}')
-              on we.whiteboard_id = w.id"
+              FROM whiteboard_elements we
+              JOIN (whiteboards w JOIN courses c
+                ON w.course_id = c.id AND c.canvas_api_domain = '${source_canvas}')
+              ON we.whiteboard_id = w.id"
 
 # If the Canvas hostname should not be changed, select all columns without changes.
 
 elif [[ "${source_canvas}" ]]; then
 
-  output_csv "assets" "select a.* from assets a
-              join courses c
-              on a.course_id = c.id and c.canvas_api_domain = '${source_canvas}'"
+  output_csv "assets" "SELECT a.* FROM assets a
+              JOIN courses c
+              ON a.course_id = c.id AND c.canvas_api_domain = '${source_canvas}'"
 
-  output_csv "asset_whiteboard_elements" "select awe.* from asset_whiteboard_elements awe
-              join (assets a join courses c
-                on a.course_id = c.id and c.canvas_api_domain = '${source_canvas}')
-              on awe.asset_id = a.id"
+  output_csv "asset_whiteboard_elements" "SELECT awe.* FROM asset_whiteboard_elements awe
+              JOIN (assets a JOIN courses c
+                ON a.course_id = c.id AND c.canvas_api_domain = '${source_canvas}')
+              ON awe.asset_id = a.id"
 
-  output_csv "courses" "select c.* from courses c
-              where c.canvas_api_domain = '${source_canvas}'"
+  output_csv "courses" "SELECT c.* FROM courses c
+              WHERE c.canvas_api_domain = '${source_canvas}'"
 
   # TODO: When course-groups feature is live, un-comment the following.
-  #  output_csv "course_groups" "select g.* from course_groups g
-  #              join courses c
-  #              on g.course_id = c.id and c.canvas_api_domain = '${source_canvas}'"
+  #  output_csv "course_groups" "SELECT g.* FROM course_groups g
+  #              JOIN courses c
+  #              ON g.course_id = c.id AND c.canvas_api_domain = '${source_canvas}'"
   #
-  #  output_csv "course_group_memberships" "select g.* from course_group_memberships g
-  #              join courses c
-  #              on g.course_id = c.id and c.canvas_api_domain = '${source_canvas}'"
+  #  output_csv "course_group_memberships" "SELECT g.* FROM course_group_memberships g
+  #              JOIN courses c
+  #              ON g.course_id = c.id AND c.canvas_api_domain = '${source_canvas}'"
 
-  output_csv "whiteboards" "select w.* from whiteboards w
-              join courses c
-              on w.course_id = c.id and c.canvas_api_domain = '${source_canvas}'"
+  output_csv "whiteboards" "SELECT w.* FROM whiteboards w
+              JOIN courses c
+              ON w.course_id = c.id AND c.canvas_api_domain = '${source_canvas}'"
 
-  output_csv "whiteboard_elements" "select we.* from whiteboard_elements we
-              join (whiteboards w join courses c
-                on w.course_id = c.id and c.canvas_api_domain = '${source_canvas}')
-              on we.whiteboard_id = w.id"
+  output_csv "whiteboard_elements" "SELECT we.* FROM whiteboard_elements we
+              JOIN (whiteboards w JOIN courses c
+                ON w.course_id = c.id AND c.canvas_api_domain = '${source_canvas}')
+              ON we.whiteboard_id = w.id"
 
 # If no source Canvas is specified, select all rows.
 
 else
 
-  output_csv "assets" "select * from assets"
-  output_csv "asset_whiteboard_elements" "select * from asset_whiteboard_elements"
-  output_csv "courses" "select * from courses"
+  output_csv "assets" "SELECT * FROM assets"
+  output_csv "asset_whiteboard_elements" "SELECT * FROM asset_whiteboard_elements"
+  output_csv "courses" "SELECT * FROM courses"
   # TODO: When course-groups feature is live, un-comment the following two lines.
-  #  output_csv "course_groups" "select * from course_groups"
-  #  output_csv "course_group_memberships" "select * from course_group_memberships"
-  output_csv "whiteboards" "select * from whiteboards"
-  output_csv "whiteboard_elements" "select * from whiteboard_elements"
+  #  output_csv "course_groups" "SELECT * FROM course_groups"
+  #  output_csv "course_group_memberships" "SELECT * FROM course_group_memberships"
+  output_csv "whiteboards" "SELECT * FROM whiteboards"
+  output_csv "whiteboard_elements" "SELECT * FROM whiteboard_elements"
 
 fi
 
 # If all tables are requested, pull the Canvas table as well.
 
 if [[ "${all_tables}" ]]; then
-  output_csv "canvas" "select * from canvas"
+  output_csv "canvas" "SELECT * FROM canvas"
 fi
 
 echo "Done."
