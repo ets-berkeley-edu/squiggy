ccbd2745eff08221ef21e747edde7fde2e1b872e
Pauline Kerschen <kerschen@berkeley.edu>
2024-09-26 17:44:28 -0700

    COL-2237 Lock down server-side fabric.js version

--- .platform/hooks/predeploy/01_install_nodejs.sh
+++ .platform/hooks/predeploy/01_install_nodejs.sh
@@ -3,6 +3,6 @@
 sudo curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
 sudo yum install -y nodejs
 sudo npm install -g typescript
-sudo npm install -g fabric
+sudo npm install -g fabric@5.3.0
 
 exit 0
